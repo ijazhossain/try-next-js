c46fb4977f6fd82f06ea73dc655d4c05a6d8938a
ijaz35 <ijaz.hossain1@gmail.com>
2023-06-26 14:24:56 +0600

    dynamic rout added

---
+++ src/app/blogs/[[...segments]]/page.js
@@ -0,0 +1,13 @@
+
+
+const page = ({ params }) => {
+    console.log(params.segments);
+    const [year, id] = params.segments || [];
+    return (
+        <div>
+            single blog of year {year || new Date().getFullYear()} with id {id}
+        </div>
+    );
+};
+
+export default page;
